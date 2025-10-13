4cb8e64120043109d9b95f3ee6ec7d0f5c210c9a
Fernando Reina Galvez <p12regaf@uco.es>
2025-10-14 01:27:43 +0200

    Eliminar comentarios de prueba de actualización en el archivo principal

--- main.py
+++ main.py
@@ -99,7 +99,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# ESTE ES EL COMMIT DE PRUEBA PARA DIAGNOSTICO
-# prueba de actualizacion IIIIIIIIII
-# prueba de actualizacion --- IGNORE ---
